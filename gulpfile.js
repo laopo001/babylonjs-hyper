157f7180ff0bdc44b96b04229183eca7a51d4970
laopo001 <353272497@qq.com>
2018-03-25 01:07:58 +0800

    修改发布方式

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,39 @@
 const gulp = require('gulp')
+const replace = require('gulp-replace')
 
-
-
-gulp.task('run', function (cb) {
-    gulp
-    .src('./build')
-    .pipe(gulp.dest('qq'));
+var co = require('co');
+var fs = require('fs');
+var OSS = require('ali-oss')
+var path = require('path');
+var client = new OSS({
+    region: 'oss-cn-shenzhen',
+    accessKeyId: 'KyVKg6tfdreplWYe',
+    accessKeySecret: 'mg9bzeiZKfCiBfi6v2KtSZMZKr2QUB',
+    bucket: 'dadigua'
 });
+
+
+
+gulp.task('replace', function (cb) {
+    gulp.src('./build/**')
+        .pipe(replace('script src="', 'script src="http://dadigua.oss-cn-shenzhen.aliyuncs.com/hypergl/deploy/'))
+        .pipe(replace('sourceMappingURL=', 'sourceMappingURL=http://dadigua.oss-cn-shenzhen.aliyuncs.com/hypergl/deploy/'))
+        .pipe(gulp.dest('deploy'))
+    cb();
+});
+
+gulp.task('upload', ['replace'], function (cb) {
+    fs.readdir(path.resolve(__dirname, './deploy'), 'utf8', function (err, files) {
+
+        co(function* () {
+            for (let i = 0; i < files.length; i++) {
+                file = files[i]
+                var result = yield client.put('hypergl/deploy/' + file, path.resolve(__dirname, './deploy/' + file));
+                console.log(result.url);
+            }
+        }).then(cb).catch(function (err) {
+            console.log(err);
+        });
+    })
+});
+
